0be9547c69283e05efff4aa7249fb8ad62ea879c
Gryhoriev Mykhailo <grigoriev1093@gmail.com>
2018-12-12 12:44:38 +0200

    Start project

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var minify = require('gulp-minify');
+var rename = require('gulp-rename');
+var cssNano = require('gulp-cssnano');
+var del = require('del');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var concat = require('gulp-concat');
+var uglyfly = require('gulp-uglyfly');
+
+/*TASK FOR DIST START*/
+gulp.task("mini-css", () => {
+    return gulp.src('./src/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(cssNano())
+        .pipe(concat('styles.min.css'))
+        .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task("mini-js", () => {
+    return gulp.src('./src/js/scripts.js')
+        .pipe(uglyfly())
+        .pipe(rename('scripts.min.js'))
+        .pipe(gulp.dest('./app/js'));
+});
+
+gulp.task("copy-js", () => {
+    return gulp.src(['./src/js/*.js', '!src/js/scripts.js'])
+        .pipe(gulp.dest('./app/js'));
+});
+
+gulp.task('mini-img', () => {
+    gulp.src('./src/img/**/*')
+        .pipe(imagemin({
+          interlaced: true,
+          progressive: true,
+          svgoPlugins: [{removeViewBox: false}],
+          use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./app/img'));
+});
+
+gulp.task('del', () => {
+    return del.sync('app');
+});
+
+gulp.task('build:app', ['del', 'mini-css', 'copy-js', 'mini-js', 'mini-img'], () => {
+    gulp.dest('./app/');
+});
+
+/*TASK FOR DIST END*/
+
+
+
+gulp.task('serve', () => {
+   browserSync.init({
+       server: ''
+   });
+
+   gulp.watch('./src/scss/**/*.scss', ['mini-css']).on('change', browserSync.reload);
+   gulp.watch('./*.html').on('change', browserSync.reload);
+   gulp.watch('./src/img/**/*.*', ['mini-img']).on('change', browserSync.reload);
+   gulp.watch('./src/js/**/*.*', ['mini-js']).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
---
+++ src/js/scripts.js
@@ -0,0 +1,133 @@
+window.onload = function() {
+  Particles.init({
+    selector: '.background',
+    maxParticles: 100,
+    connectParticles: true,
+    color: '#ffffff',
+
+    responsive: [
+    {
+    	breakpoint: 940,
+    	options: {
+    		maxParticles: 60
+    	}
+    }, {
+    	breakpoint: 768,
+    	options: {
+    		maxParticles: 30
+    	}
+    }]
+  });
+};
+
+function openWindow(elem) {
+	var elem = document.getElementById(elem);
+	elem.style.display = "flex";
+}
+
+function openMenu(elem, menuId) {
+	var menuId = document.getElementById(menuId);
+	elem.classList.toggle("change");
+	menuId.classList.toggle("menu-hide");
+}
+
+function animateOnClick(elem, type, animation) {
+	elem.classList.add(type, animation);
+	
+	setTimeout(function() {
+		elem.classList.remove(type, animation);
+	}, 1000);
+}
+
+$(document).mouseup(function(elem) {
+	var close = $(".close");
+	var overlayer = $('.overlayer');
+	var mail = $('.mail');
+
+	if (elem.target != close[0] && close.has(elem.target).length === 0) {
+		if (overlayer) {
+			overlayer.fadeOut();
+		}
+
+		if (mail) {
+			mail.delay(2500).fadeOut();
+		}
+	}	
+});
+
+$(".close").click(function() {
+	$(".overlayer").fadeOut();
+});
+
+$(document).ready(function() {
+	$("#phone").mask("+38(099) 999 99 99");
+});
+
+$(document).on("click", "#play-btn", function() {
+	var video = $("#player");
+	var src = video.attr("src");
+	video.attr("src", src + "&autoplay=1");
+});
+
+$(document).ready(function() {
+	$("#menuNav, #menu-mobile, #arrow").on("click", "a", function(event) {
+		event.preventDefault();
+
+		var id = $(this).attr("href");
+		var top = $(id).offset().top;
+		var btn = $('.burger');
+		var menu = $('#menu-mobile');
+
+		$("body, html").animate({scrollTop: top},1500);
+		menu.addClass('menu-hide');
+		btn.removeClass('change');
+	});
+});
+
+$('#formMain').validate({
+	rules: {
+		name: "required",
+		phone: "required",
+		email: {
+			required: true,
+			email: true
+		}
+	},
+	focusCleanup: true,
+	focusInvalid: false
+});
+
+$(document).ready(function() {
+	$("#formMain").submit(function() {
+		var id = $(this);
+		$.ajax({
+			type: "POST",
+			url: "../../send.php",
+			data: id.serialize()
+		}).done(function(response) {
+			var resultId = document.getElementById("messegeResult");
+			resultId.style.display = "flex";
+	    resultId.innerHTML = response;
+	    setTimeout(function() {
+	    	id.trigger("reset");
+	    }, 1000);
+		});
+		return false;
+	});
+
+});
+
+$(document).ready(function() {
+	$(".benefits-description .benefits-text").hide().prev().click(function() {
+		$(".benefits-description .benefits-text").not(this).slideUp();
+
+		if ($(this).hasClass("active")) {
+				$(this).toggleClass("active");
+			} else {
+				$(".benefits-title").removeClass("active");
+				$(this).toggleClass("active");
+			}
+
+		$(this).next().not(":visible").slideDown();
+	})
+});
